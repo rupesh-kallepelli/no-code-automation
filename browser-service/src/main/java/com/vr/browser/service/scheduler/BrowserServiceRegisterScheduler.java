24efcd8878938b0d90379c4359ab3afd87759c35
rupesh_kallepelli <kallepallirupesh@gmail.com>
2026-02-01 18:04:56 +0530

    updated websocket browser proxy and browser implementation

--- browser-service/src/main/java/com/vr/browser/service/scheduler/BrowserServiceRegisterScheduler.java
+++ browser-service/src/main/java/com/vr/browser/service/scheduler/BrowserServiceRegisterScheduler.java
@@ -38,7 +38,7 @@ public class BrowserServiceRegisterScheduler {
         this.browserRegistry = browserRegistry;
     }
 
-//    @Scheduled(fixedRate = 3000)
+    @Scheduled(fixedRate = 3000)
     public void register() {
         registryClient.
                 post()
@@ -57,7 +57,7 @@ public class BrowserServiceRegisterScheduler {
                 .block();
     }
 
-//    @PostConstruct
+    @PostConstruct
     public void init() {
         RegistryResponse registryResponse = registryClient.
                 post()
